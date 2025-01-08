999ffe5cb43e7e91e0da16bf6fce338357bc807d
yandiux <yandiuxx@gmail.com>
2025-01-07 21:11:59 -0500

    create board function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("test");
+// thid to avoid everything from crashind down and give some time to
+// the DOM to load before the script is executed
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM fully loaded and parsed");
+  let inputSize = document.querySelector("#input-size");
+  createBoard(16);
+});
+
+function createBoard(size) {
+  let board = document.querySelector(".container");
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  let numDivs = size * size;
+  for (let i = 0; i < size * size; i++) {
+    let div = document.createElement("div");
+    div.style.backgroundColor = "yellow";
+    board.insertAdjacentElement("beforeend", div);
+  }
+}
